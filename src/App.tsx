8bd807a3a14974072c6399fce71d982acd777a3c
Asher Lim <asherlimyi@gmail.com>
2023-01-05 10:08:39 +0800

    Configure login to use MPC

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
-import { Web3Auth } from "@web3auth/modal";
 import { WALLET_ADAPTERS, CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import { Web3Auth } from "@web3auth-mpc/web3auth";
+import { OpenloginAdapter } from "@web3auth-mpc/openlogin-adapter";
+import { tssDataCallback, tssGetPublic, tssSign, generatePrecompute } from "torus-mpc";
 import RPC from "./web3RPC";
 import "./App.css";
-import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
 
 //from https://dashboard.web3auth.io
 const clientId = "BCxzKbjFiYzK7weDw6Wsxa1C5CS7W8OhsylVxyC9RI7Iw_zFR4gGpe36cG4c44cDBa0BVLnjABLhU_BSIadNF3c"; 
+const rpcTarget = "https://eth-goerli.g.alchemy.com/v2/eq_jLeOuH6iTZIL1u51ha6h2yODGGeAo";
 
 function App() {
   const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
@@ -16,30 +18,75 @@ function App() {
   useEffect(() => {
     const init = async () => {
       try {
+        // const web3auth = new Web3Auth({
+        //   clientId,
+        //   chainConfig: {
+        //     chainNamespace: CHAIN_NAMESPACES.EIP155,
+        //     chainId: "0x5",
+        //     rpcTarget: "https://eth-goerli.g.alchemy.com/v2/eq_jLeOuH6iTZIL1u51ha6h2yODGGeAo"
+        //   },
+        // });
+
         const web3auth = new Web3Auth({
-          clientId,
+          clientId: clientId,
           chainConfig: {
-            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainNamespace: "eip155",
             chainId: "0x5",
-            rpcTarget: "https://eth-goerli.g.alchemy.com/v2/eq_jLeOuH6iTZIL1u51ha6h2yODGGeAo"
+            rpcTarget: rpcTarget,
+            displayName: "Goerli Testnet",
+            blockExplorer: "https://goerli.etherscan.io/",
+            ticker: "ETH",
+            tickerName: "Ethereum",
           },
         });
 
         //SET MFA TO MANDATORY
         const openloginAdapter = new OpenloginAdapter({
+          // Multi Factor Authentication has to be mandatory
           loginSettings: {
             mfaLevel: "mandatory",
+          },
+          // TSS Settings needed for TSS implementation
+          tssSettings: {
+            useTSS: true,
+            tssGetPublic,
+            tssSign,
+            tssDataCallback,
+          },
+          adapterSettings: {
+            // points to the beta mpc network containing TSS implementation
+            _iframeUrl: "https://mpc-beta.openlogin.com",
+            // network has to be development
+            network: "development",
+            clientId: clientId, // Client ID from your Web3Auth Dashboard
           },
         });
         web3auth.configureAdapter(openloginAdapter);
 
         setWeb3auth(web3auth);
 
-        await web3auth.initModal();
-          if (web3auth.provider) {
-            setProvider(web3auth.provider);
-          };
+        //Initialise Web3Auth
+        await web3auth.initModal({
+          // config to remove the external wallet adapters
+          modalConfig: {
+            "torus-evm": {
+              label: "Torus Wallet",
+              showOnModal: false,
+            },
+            metamask: {
+              label: "Metamask",
+              showOnModal: false,
+            },
+            "wallet-connect-v1": {
+              label: "Wallet Connect",
+              showOnModal: false,
+            },
+          },
+        });
 
+        if (web3auth.provider) {
+          setProvider(web3auth.provider);
+        };
       } catch (error) {
         console.error(error);
       }
@@ -55,6 +102,7 @@ function App() {
     }
     const web3authProvider = await web3auth.connect();
     setProvider(web3authProvider);
+    generatePrecompute();
   };
 
   const getUserEmail = async () => {
